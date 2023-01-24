214ac449a1b28f42872697cb2f4483dbb5ebc1f3
Simon Lyngshede <slyngshede@wikimedia.org>
2023-01-24 10:00:06 +0100

    Switch to built in LogoutView.
    Use the LogoutView, rather than relying to the
    admin logout view.
    
    Change-Id: I5efb45deb7cf27db5287bfb4a0e5b2c2003af0f8

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'accounts'
 urlpatterns = [
     path('', login_required(TemplateView.as_view(template_name='overview.html')), name='overview'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
