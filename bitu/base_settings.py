c78fea879be44b1c50e6d77d97b2a221163e2fb5
Simon Lyngshede <slyngshede@wikimedia.org>
2023-02-24 09:08:18 +0100

    LOGIN: Add custom WikiMedia SSO login page.
    Switching between login pages must be done in the
    settings, using LOGIN_URL.
    
    Change-Id: I2696e3287de4263133be21a2724a3c0e56bc9d92

--- bitu/base_settings.py
+++ bitu/base_settings.py
@@ -118,4 +118,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 SIGNUP_USERNAME_VALIDATORS = ['ldapbackend.validators.LDAPUsernameValidator', 'ldapbackend.validators.unix_username_validator']
 LOGIN_REDIRECT_URL = 'accounts:overview'
-LOGOUT_REDIRECT_URL = 'accounts:login'
+LOGOUT_REDIRECT_URL = 'wikimedia:login'
--- wikimedia/urls.py
+++ wikimedia/urls.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-
-from .views import MediaWikiLinkView
+from django.views.generic import TemplateView
 
 
 app_name = 'wikimedia'
-urlpatterns = []
+urlpatterns = [
+    path('login/', TemplateView.as_view(template_name="oidc_login.html"), name="login")
+]
