e5247c23736f8b9876c3ccec03cc2905fbefe2f7
Simon Lyngshede <slyngshede@wikimedia.org>
2023-05-11 10:33:38 +0200

    signup:blocklist Expand blocklist feature
    * Fetch blocklist from meta.wikimedia.org.
    * Allow blocks to expire.
    * Add origin field to blocklist items.
    
    Change-Id: I63dde3e14874de970fe7dc83ce5d914f4eedb253

--- wikimedia/management/commands/blocklist_wmf.py
+++ wikimedia/management/commands/blocklist_wmf.py
@@ -1,13 +1,27 @@
+from datetime import timedelta
+
 import requests
 
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
+from django.utils import timezone
+
 from signups.models import BlockListUsername
 
 
 class Command(BaseCommand):
+    name = 'wmf_blocklist'
+    max_age_days = 7
     help = 'Import username blocklist from Wikimedia'
-    BLOCK_LIST_URI = 'https://wikitech.wikimedia.org/w/api.php?action=parse&formatversion=2&page=MediaWiki%3ATitleblacklist&prop=text&format=json'
+    sources = [
+        {
+            'name': 'wikitech',
+            'uri': 'https://wikitech.wikimedia.org/w/api.php?action=parse&formatversion=2&page=MediaWiki%3ATitleblacklist&prop=text&format=json'
+        },
+        {
+            'name': 'meta',
+            'uri': 'https://meta.wikimedia.org/w/api.php?action=parse&formatversion=2&page=Title_blacklist&format=json'
+        }]
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -17,7 +31,26 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        response = requests.get(self.BLOCK_LIST_URI)
+        check_mode = options.get('check', False)
+
+        for blocklist in self.sources:
+            self.fetch_blocklist(blocklist['name'], blocklist['uri'], check_mode)
+
+        self.expire(check_mode)
+
+    def expire(self, check_mode):
+        max_age = timezone.now() - timedelta(days=self.max_age_days)
+        blocks = BlockListUsername.objects.filter(origin=self.name, last_modified__lt=max_age).all()
+
+        if check_mode:
+            for block in blocks:
+                print(f' will delete: {block.regex}')
+            return
+
+        blocks.delete()
+
+    def fetch_blocklist(self, name, uri, check_mode):
+        response = requests.get(uri)
         content = response.json()['parse']['text']
         soup = BeautifulSoup(content, 'html.parser')
 
@@ -27,10 +60,18 @@ class Command(BaseCommand):
                     continue
 
                 regex = line.split(' ')[0]
-                if options['check']:
+                if check_mode and regex:
                     print(regex)
                     continue
-                BlockListUsername.objects.get_or_create(
+
+                block, created = BlockListUsername.objects.get_or_create(
                     regex=regex,
-                    comment='Import from meta.wikimedia.org'
+                    comment=f'Import from {name}',
+                    origin=self.name
                 )
+
+                # Creating a new blocklist item will automatically updated
+                # last modified. For existing items, call save() to update
+                # last modified timestamp.
+                if not created:
+                    block.save()
