09f950c2127d537102ada95cd79b74cf6c218fee
Simon Lyngshede <slyngshede@wikimedia.org>
2024-02-05 16:34:56 +0100

    CI Fix broken tests.
    * A number of validator tests where failing.
    * Rework requirements for dev and CI.
    * distutils is deprecated, but now ships with setuptools, add as tox
      dependency.
    * Remove mariadb/mysql dependency
    
    Bug: T355172
    
    Change-Id: Iec96e2e6eb20dff2e5e8b989c9196666127aa12e

--- src/bitu/signups/tests/test_validators.py
+++ src/bitu/signups/tests/test_validators.py
@@ -2,13 +2,13 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from signups.models import BlockListUsername
-from signups.validators import IsDomainValidator, UsernameValidator
+from signups.validators import IsURLValidator, UsernameValidator
 
 class UsernameValidators(TestCase):
     def test_is_domain(self):
-        IsDomainValidator('plain username')
-        IsDomainValidator('www.dev')
-        self.assertRaises(ValidationError, IsDomainValidator, 'https://en.wikipedia.org')
+        IsURLValidator('plain username')
+        IsURLValidator('www.dev')
+        self.assertRaises(ValidationError, IsURLValidator, 'https://en.wikipedia.org')
 
 
     def test_blocklist(self):
@@ -16,21 +16,21 @@ class UsernameValidators(TestCase):
         UsernameValidator('Anne Ancher')
         UsernameValidator('Peder Severin Krøyer')
 
-        block = BlockListUsername(regex='M\w* Ancher')
+        block = BlockListUsername(regex=r'M\w* Ancher')
         block.save()
 
         self.assertRaises(ValidationError, UsernameValidator, 'Michael Ancher')
         UsernameValidator('Anne Ancher')
         UsernameValidator('Peder Severin Krøyer')
 
-        block = BlockListUsername(regex='\w* Ancher')
+        block = BlockListUsername(regex=r'\w* Ancher')
         block.save()
 
         self.assertRaises(ValidationError, UsernameValidator, 'Michael Ancher')
         self.assertRaises(ValidationError, UsernameValidator, 'Anne Ancher')
         UsernameValidator('Peder Severin Krøyer')
 
-        block = BlockListUsername(regex='\w* Severin \w*')
+        block = BlockListUsername(regex=r'\w* Severin \w*')
         block.save()
 
         self.assertRaises(ValidationError, UsernameValidator, 'Peder Severin Krøyer')
