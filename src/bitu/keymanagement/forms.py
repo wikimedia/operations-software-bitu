079d4392b634ce5d730e680c8ce59a30221aca6b
Simon Lyngshede <slyngshede@wikimedia.org>
2024-03-27 08:57:28 +0100

    Usability improvements for SSH key management.
    * Extract comment from SSH key.
    * Link to SSH key creation guide.
    * List valid SSH key algorithms.
    
    Bug: T359536
    Change-Id: I326debae7e1694de7da29d6a8cf6434b88adae3e

--- src/bitu/keymanagement/forms.py
+++ src/bitu/keymanagement/forms.py
@@ -1,4 +1,9 @@
 from django import forms
+from django.conf import settings
+from django.utils.safestring import mark_safe
+from django.utils.text import format_lazy
+from django.utils.translation import gettext_lazy as _
+
 
 from .models import SSHKey
 
@@ -12,6 +17,18 @@ class SSHKeyCreateForm(forms.ModelForm):
             field = self.fields[field_name]
             field.widget.attrs['class'] = 'cdx-text-input__input'
             field.widget.attrs['aria-describedby'] = f'cdx-{field_name}'
+
+        allowed_key_types = getattr(settings, 'BITU_SSH_KEY_VALIDATOR',{}).get('allowed_key_type', {})
+
+
+        help_messages = [
+            _('Please consult our documentation if you need help generating your SSH keys, \
+              available at: <a href="https://wikitech.wikimedia.org/wiki/Generate_an_SSH_Key"> \
+              https://wikitech.wikimedia.org/wiki/Generate_an_SSH_Key</a>').__str__(),
+            _("Valid SSH key types are %(key_types)s" % {"key_types": ', '.join(allowed_key_types.keys())}).__str__(),
+        ]
+
+        self.fields['ssh_public_key'].help_text = mark_safe('<br />'.join(help_messages))
 
     class Meta:
         model = SSHKey
