21cf7105f54afbff8c32ff0d5a17ca99da45b628
Antoine Musso <hashar@free.fr>
2025-03-26 19:09:57 +0100

    Adjust build.sh for other environments
    On a Linux x86_64 I encountered a build failure:
    
        .buildkit_qemu_emulator: /bin/sh:
        Invalid ELF image for this architecture
    
    Pass to `--platform` the machine name from `uname -m` though I would
    expect Docker to do it by default?
    
    The `testing` Blubber variant has been renamed `test`.
    
    Prefix the image tag with `localhost/` to make it clear it is a local
    build image and not one that is published or coming from a remote
    registry.
    
    Publish the image upon building (`--load`), Docker does it automatically
    but that is not the case when using an alternative system such as
    Podman.
    
    Change-Id: I0820ee36a8d328b1433315fa09df6a1bfcd7cfb9

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+MACHINE_NAME=$(uname -m)
+
 DOCKER_BUILDKIT=1 docker build \
     --pull=false \
     -f .pipeline/blubber.yaml \
-    --target testing \
-    --platform linux/arm64 \
-    -t bitu/testing .
+    --target test \
+    --platform "linux/$MACHINE_NAME" \
+    --load \
+    --tag localhost/bitu-testing \
+    .
