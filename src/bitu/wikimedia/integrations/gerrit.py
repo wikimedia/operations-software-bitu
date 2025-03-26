f16647298b9f7b058236b3fb546662a45848fcfe
Antoine Musso <hashar@free.fr>
2025-03-26 23:36:10 +0100

    Fix handling of status code in Gerrit integration
    The Gerrit integration would only give a success when unblocking an user
    that is not blocked (== active). In every other cases, it will error out
    which is confusing to the admin doing the action.
    
    The code seems to have taken in account the trick I have found to query
    for both active and inactive users but missed the status code handling.
    Bryan and I worked on it a year ago via T307558:
    
    wikitech: Update Gerrit blocking logic
    I65d7bf0d47a39828830255bdca56f0709f6698d4
    
    wikitech: allow unblocking inactive accounts
    I40979e82d191bf8c6025cd7037257e800016e135
    
    wikitech: fix handling of Gerrit status code
    Id080b1a4f1166680156947348fa2e5fb0c1743d6
    
    Change-Id: I5fc0191350507978adc8dacb2bd3d6ec1a9a99f9

--- src/bitu/wikimedia/integrations/gerrit.py
+++ src/bitu/wikimedia/integrations/gerrit.py
@@ -31,35 +31,81 @@ class Gerrit():
         )
 
     def _lookup_user(self, uid):
+        # By default Gerrit only matches active accounts (by automatically
+        # adding `is:active`. Adding both ensure we get the account regardless
+        # of its state.
+        # https://phabricator.wikimedia.org/T307558#9625736
         url = f'{self.base_url}/r/a/accounts/?n=1&q=(is:inactive+OR+is:active)+username:{quote(uid)}'
+
         resp = requests.get(url, auth=self.auth, headers=self.headers)
         if resp.status_code != 200:
             logger.warning(f'Gerrit user lookup failed, username:{uid}, message: {resp.text}, http_code: {resp.status_code}')
             raise Exception('Gerrit user lookup failed')
 
-        data = Gerrit.loadResponse(resp)
+        accounts = Gerrit.loadResponse(resp)
 
-        if len(data) > 1:
-            logger.error('Found multiple users in Gerrit, query: {uid}')
-            raise Exception('Gerrit returned multiple users, single ID expected.')
-        elif len(data) == 0:
-            logger.warning(f'Gerrit user lookup failed, username:{uid}')
+        # The API returns a list of AccountInfo entities and we limited it to 1 entry
+        # https://gerrit.wikimedia.org/r/Documentation/rest-api-accounts.html#query-account
+        if not accounts:
+            logger.warning(f'Gerrit user not found, username:{uid}')
             raise Exception('Gerrit user not found')
 
-        if '_account_id' in data[0]:
-            return data[0]['_account_id']
+        account = accounts.pop()
 
-        raise Exception('Gerrit user lookup failed')
+        if '_account_id' not in account:
+            logger.error('Gerrit user lookup failed, no _account_id, username:{uid}')
+            raise Exception('Gerrit user lookup failed')
+
+        return account['_account_id']
 
     def update_user(self, uid, block):
-        gerrit_id = self._lookup_user(uid)
+        gerrit_account_id = self._lookup_user(uid)
+        user = "%s (id: %s)" % (uid, gerrit_account_id)
+
+        # The Gerrit API relies on PUT / DELETE to, respectively, activate or
+        # deactivate an account.
         method = requests.delete if block else requests.put
-        url = f'{self.base_url}/r/a/accounts/{gerrit_id}/active'
+        url = f'{self.base_url}/r/a/accounts/{gerrit_account_id}/active'
+
+        # The response is solely reflected by the status code.
         resp = method(url, auth=self.auth)
-        logger.info(f'Update user in Gerrit, username: {uid}, gerrit_id: {gerrit_id}, block: {block}, http_status: {resp.status_code}, message: {resp.text}')
-        if resp.status_code not in [201, 204, 409]:
-            logger.warning(f'Failed to update user in Gerrit, username: {uid}, gerrit_id: {gerrit_id}, status: {resp.status_code}, message: {resp.text}, block: {block}')
-            raise Exception('Failed to updated user in Gerrit')
+
+        # DELETE to make an account inactive, the responses are:
+        # - 204 account successfully made inactive (blocked)
+        # - 409 account was already inactive (noop)
+        #
+        #
+        # PUT to make an account active, the responses are:
+        # - 200 account was already active (noop)
+        # - 201 account becomes active (unblocked)
+        #
+        #
+        # See also https://gerrit.wikimedia.org/r/c/operations/mediawiki-config/+/1011151
+
+        resp_infos = f'http_status: {resp.status_code}, message: {resp.text}'
+        status_code = resp.status_code
+
+        # DELETE to make an account inactive
+        # https://gerrit.wikimedia.org/r/Documentation/rest-api-accounts.html#delete-active
+        if block:
+            if status_code == 204:
+                logger.info(f'{user} is now blocked in Gerrit')
+            elif status_code == 409:
+                logger.info(f'{user} is already blocked in Gerrit')
+            else:
+                logger.error(f'Failed to block user in Gerrit. {resp_infos}')
+                raise Exception('Failed to block user in Gerrit.')
+
+        # PUT to make an account active
+        # https://gerrit.wikimedia.org/r/Documentation/rest-api-accounts.html#set-active
+        else:
+            if status_code == 200:
+                logger.info(f'{user} was not blocked in Gerrit')
+            elif status_code == 201:
+                logger.info(f'{user} is now active in Gerrit')
+            else:
+                logger.error(f'Failed to unblock user in Gerrit. {resp_infos}')
+                raise Exception('Failed to unblock user in Gerrit.')
 
     def block_user(self, uid):
         self.update_user(uid, block=True)
---
+++ src/bitu/wikimedia/integrations/tests/test_integrations.py
@@ -0,0 +1,150 @@
+import json
+from unittest.mock import patch, Mock, ANY
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase, override_settings
+from django.urls import reverse
+
+import bituldap
+
+from ldapbackend.tests import dummy_ldap
+
+User = get_user_model()
+
+
+class TestIntegrations(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        dummy_ldap.setup()
+
+        admin, _ = User.objects.get_or_create(username='admin')
+        admin.set_password('secret')
+        admin.save()
+
+        user = bituldap.new_user(uid='jdoe')
+        user.cn = 'Jane Doe'
+        user.mail = 'jane.doe@example.org'
+        # Mandatories:
+        user.sn = 'some sn'
+        user.uidNumber = 1911
+        user.gidNumber = 777
+        user.homeDirectory = '/home/jdoe'
+        user.entry_commit_changes()
+
+        cls.user_entry = user
+
+    def setUp(self):
+        self.as_admin = Client()
+        self.as_admin.login(username='admin', password='secret')
+
+    def do(self, action, client_name):
+        block_action_url = reverse(
+            'wikimedia:%s' % action,
+            kwargs={'username': 'jdoe'},
+        )
+
+        with (
+            self.settings(
+                ACCOUNT_MANAGERS=['admin'],
+                USER_AGENT='Bitu User Agent (test)',
+                GERRIT={
+                    'user': 'gerritbituuser',
+                    'password': 'secret',
+                    'base_url': 'https://review.example.org/r',
+                },
+                GITLAB={
+                    'base_url': 'https://gitlab.example.org',
+                    'token': '42',
+                },
+                PHABRICATOR_API_TOKEN='99',
+                PHABRICATOR_URI='https://phab.example.org',
+            ),
+            self.assertLogs('bitu', level='INFO') as logs,
+        ):
+            response = self.as_admin.post(block_action_url, {
+                'action': action,
+                'comment': 'doing action: %s' % action,
+                'created_by': 'admin',
+                'username': 'jdoe',
+            })
+            self.assertRedirects(
+                response,
+                expected_url='/wikimedia/block/?q=jdoe',
+                fetch_redirect_response=False,
+            )
+        return logs
+
+    @override_settings(_UPDATE_ACCOUNT_CLIENTS_NAMES='ldap')
+    @override_settings(BITU_LDAP={'users': {'dn': 'ou=people,dc=example,dc=org'}})
+    def test_ldap__update_user(self):
+        self.skipTest('Not implemented yet')
+
+    @override_settings(_UPDATE_ACCOUNT_CLIENTS_NAMES='gerrit')
+    @patch('wikimedia.integrations.gerrit.requests')
+    def test_gerrit__update_user(self, requests):
+        requests.get.return_value = Mock(
+            status_code=200,
+            text=')]}\'' + json.dumps([{'_account_id': 7777}]),
+        )
+
+        cases = {
+            'blocking': {
+                'update_api': requests.delete,
+                'returns': 204,
+                'action': 'block_user',
+                'expected_log': 'INFO:bitu:Jane Doe (id: 7777) is now blocked in Gerrit',
+            },
+            'already blocked': {
+                'update_api': requests.delete,
+                'returns': 409,
+                'action': 'block_user',
+                'expected_log': 'INFO:bitu:Jane Doe (id: 7777) is already blocked in Gerrit',
+            },
+            'blocking action fails': {
+                'update_api': requests.delete,
+                'returns': 503,
+                'action': 'block_user',
+                'expected_log': 'ERROR:bitu:Failed to block user in Gerrit. http_status: 503, message: ',
+            },
+
+            'unblock': {
+                'update_api': requests.put,
+                'returns': 200,
+                'action': 'unblock_user',
+                'expected_log': 'INFO:bitu:Jane Doe (id: 7777) was not blocked in Gerrit',
+            },
+            'already unblocked': {
+                'update_api': requests.put,
+                'returns': 201,
+                'action': 'unblock_user',
+                'expected_log': 'INFO:bitu:Jane Doe (id: 7777) is now active in Gerrit',
+            },
+            'unblocking action fails': {
+                'update_api': requests.put,
+                'returns': 503,
+                'action': 'unblock_user',
+                'expected_log': 'ERROR:bitu:Failed to unblock user in Gerrit. http_status: 503, message: ',
+            },
+        }
+
+        for title, p in cases.items():
+            with self.subTest(title=title):
+                # Configure the request
+                p['update_api'].return_value = Mock(status_code=p['returns'], text='')
+
+                logs = self.do(p['action'], 'gerrit')
+
+                # Assertions
+                p['update_api'].assert_called_with('https://review.example.org/r/r/a/accounts/7777/active', auth=ANY)
+                self.assertIn(p['expected_log'], logs.output)
+
+    @override_settings(_UPDATE_ACCOUNT_CLIENTS_NAMES='gitlab')
+    @patch('wikimedia.integrations.gitlab.requests')
+    def test_gitlab__update_user(self, requests):
+        self.skipTest('Not implement yet')
+
+    @override_settings(_UPDATE_ACCOUNT_CLIENTS_NAMES='phabricator')
+    @patch('wikimedia.integrations.phabricator.requests')
+    def test_phabricator__update_user(self, requests):
+        self.skipTest('Not implement yet')
