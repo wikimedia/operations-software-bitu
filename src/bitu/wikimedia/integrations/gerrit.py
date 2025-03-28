9df9fc422aaada06ef878705da109737dde07977
Antoine Musso <hashar@free.fr>
2025-03-28 16:36:01 +0100

    Fix removal of Gerrit json prefix
    str.strip() removes the leading AND trailing characters from the given
    list of characters. This notably caused the end of a json structure to
    be stripped and prevent any response from being deserialized.
    
    Ensure responses always have the prefix.
    Use str.removeprefix() which was added with Python 3.9.
    Add basic test coverage.
    
    Change-Id: Ie5134de12c54aab8b30e58c61a27cc58a359fb25

--- src/bitu/wikimedia/integrations/gerrit.py
+++ src/bitu/wikimedia/integrations/gerrit.py
@@ -13,10 +13,22 @@ logger = logging.getLogger('bitu')
 
 
 class Gerrit():
+
+    JSON_MAGIC = ')]}\''
+
     def __init__(self) -> None:
         self.auth = HTTPBasicAuth(settings.GERRIT['user'], settings.GERRIT['password'])
         self.base_url = settings.GERRIT['base_url']
         self.headers = {'User-Agent': settings.USER_AGENT,}
+
+    @staticmethod
+    def loadResponse(resp):
+        if not resp.text.startswith(Gerrit.JSON_MAGIC):
+            raise Exception('Json magic prefix could not be found in response')
+
+        return json.loads(
+            resp.text.removeprefix(Gerrit.JSON_MAGIC)
+        )
 
     def _lookup_user(self, uid):
         url = f'{self.base_url}/r/a/accounts/?n=1&q=(is:inactive+OR+is:active)+username:{quote(uid)}'
@@ -25,8 +37,8 @@ class Gerrit():
             logger.warning(f'Gerrit user lookup failed, username:{uid}, message: {resp.text}, http_code: {resp.status_code}')
             raise Exception('Gerrit user lookup failed')
 
-        #Gerrit returns defective JSON
-        data = json.loads(resp.text.strip(')]}\''))
+        data = Gerrit.loadResponse(resp)
+
         if len(data) > 1:
             logger.error('Found multiple users in Gerrit, query: {uid}')
             raise Exception('Gerrit returned multiple users, single ID expected.')
