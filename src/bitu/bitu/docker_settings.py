4d295ca305c5796e3eaeef74b22c4793572c403d
Perry Fraser <perry@frasers.org>
2026-07-29 14:31:38 -0400

    Use os.environ.get for checking DJANGO_DEBUGGING
    getattr doesn't work on os.environ, so setting DJANGO_DEBUGGING
    wouldn't then set settings.DEBUG to true as expected.
    
    Change-Id: I601a347a6620a4b46e096dc32ac368c66ba3eb93

--- src/bitu/bitu/docker_settings.py
+++ src/bitu/bitu/docker_settings.py
@@ -72,7 +72,7 @@ INSTALLED_APPS = INSTALLED_APPS + [
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getattr(os.environ, 'DJANGO_DEBUGGING', False)
+DEBUG = os.environ.get('DJANGO_DEBUGGING', False)
 TESTING = True
 ALLOWED_HOSTS = allowed_hosts
 
